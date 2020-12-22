c315bad0502839e69ccd4d8e282667df5ad19777
Gabriela <67986617+gabyross@users.noreply.github.com>
2020-12-22 13:42:48 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 new Vue({
     el: '#vue-app',
     data: {
-        characters: ['Mario', 'Luigi', 'Yoshi', 'Browser'],
-        ninjas: [
-          {name:'Ryu', age:15},
-          {name:'Yoshi', age:25},
-          {name:'Ken', age:35}
-        ]
+        error: false,
+        success: false
     },
     methods: {
 
