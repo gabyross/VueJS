2c7adc87576b5f65ccccccce6c5d62b809e3a61a
Gabriela <67986617+gabyross@users.noreply.github.com>
2020-12-22 13:37:42 -0400

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <ninjas></ninjas> <!--globally and locally-->
+    <p>{{ greeting() }}</p>
   </div>
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue';
-
 export default {
-  components: {
-    'ninjas': Ninjas
-    },
   data() {
     return {
-      title: 'Ninja App'
+      title: 'Your first Vue file, wooo!'
+    }
+  },
+
+  methods:{
+    greeting: function(){
+      return 'heeey cowgirl!';
     }
   }
 }
 </script>
 
-<style scoped>
-h1{
-    color: purple;
-}
+<style>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-//import Ninjas from './Ninja.vue' --- globally
-import store from './store'
-
-Vue.config.productionTip = false
-//Vue.component('ninjas', Ninjas) --- globally 
 
 new Vue({
-  store,
+  el: '#app',
   render: h => h(App)
-}).$mount('#app')
+})
