dd3924fdaf77a070d4928d977125586636c4cad5
Gabriela <67986617+gabyross@users.noreply.github.com>
2020-12-22 13:45:35 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 new Vue({
     el: '#vue-app',
     data: {
-        a: 0,
-        b: 0,
-        age: 20
+        name: '',
+        age: ''
     },
     methods: {
-        /*addToA: function(){
-            console.log('addToA');
-            return this.a + this.age;
+        logName: function(){
+            console.log('you entered your name');
         },
-        addToB: function(){
-            console.log('addToB');
-            return this.b + this.age;
-        }*/
-    },
-    computed: {
-        addToA: function(){
-            console.log('addToA');
-            return this.a + this.age;
-        },
-        addToB: function(){
-            console.log('addToB');
-            return this.b + this.age;
+        logAge: function(){
+            console.log('you entered your age');
         }
     }
 });
