ced4979b98f550785a1194ca594743190da9472b
Gabriela <67986617+gabyross@users.noreply.github.com>
2020-12-22 13:41:54 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 new Vue({
-  el: '#vue-app',
-  data: {
-      health: 100,
-      ended: false
-  },
-  methods: {
-      punch: function(){
-          this.health -= 10;
-          if ( this.health <= 0 ){
-              this.ended = true;
-          }
-      },
-      restart: function(){
-          this.health = 100;
-          this.ended = false;
-      }
-  },
-  computed: {
+    el: '#vue-app',
+    data: {
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Browser'],
+        ninjas: [
+          {name:'Ryu', age:15},
+          {name:'Yoshi', age:25},
+          {name:'Ken', age:35}
+        ]
+    },
+    methods: {
 
-  }
+    },
+    computed: {
+
+    }
 });
