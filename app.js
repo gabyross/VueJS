2be706970f79fc4453b0b2ed80665479f0248afd
Gabriela <67986617+gabyross@users.noreply.github.com>
2020-12-22 13:39:24 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-new Vue({
-  el: '#vue-app-one',
+var one = new Vue({
+    el: '#vue-app-one',
+    data: {
+      title: 'Vue App One'
+    },
+    methods: {
+
+    },
+    computed: {
+      greet: function(){
+        return 'Hello from app one';
+      }
+    }
+});
+
+var two = new Vue({
+  el: '#vue-app-two',
   data: {
-      output: 'Your favourite food'
+    title:'Vue App Two'
   },
   methods: {
-      readRefs: function(){
-          console.log(this.$refs);
-          this.output = this.$refs.input.value;
-      }
+    changeTitle: function(){
+      one.title = "title changed";
+    }
+  },
+  computed: {
+    greet: function(){
+      return 'Hello from app two';
+    }
   }
 });
